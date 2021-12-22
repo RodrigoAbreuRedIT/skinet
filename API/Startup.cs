07205b4c1b99df21d644fa1db172ac22f2e18b85
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-12-22 15:32:14 +0000

    End of section 5

--- API/Startup.cs
+++ API/Startup.cs
@@ -1,48 +1,36 @@
+using API.Extensions;
 using API.Helpers;
-using Core.Interfaces;
+using API.Middleware;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
 
-namespace API
-{
-    public class Startup
-    {
+namespace API {
+    public class Startup {
         private IConfiguration _Configuration { get; }
-        public Startup(IConfiguration configuration)
-        {
+        public Startup(IConfiguration configuration) {
             this._Configuration = configuration;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            // Estamos a injectar este serviço na aplicação
-            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+        public void ConfigureServices(IServiceCollection services) {
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<StoreContext>(x => x.UseSqlite(_Configuration.GetConnectionString("DefaultConnction")));
+            services.AddDbContext<StoreContext>(x => 
+                x.UseSqlite(_Configuration.GetConnectionString("DefaultConnction")));
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();
+            
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
-            }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
 
             app.UseHttpsRedirection();
 
@@ -51,8 +39,9 @@ namespace API
 
             app.UseAuthorization();
 
-            app.UseEndpoints(endpoints =>
-            {
+            app.UseSwaggerDocumention();
+
+            app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
             });
         }
