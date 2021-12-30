f41d379f2063b54dc9733f2554f37c55105c3128
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-12-30 14:23:30 +0000

    End of section 17

---
+++ client/src/app/account/account.service.ts
@@ -0,0 +1,71 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { BehaviorSubject, map, of, ReplaySubject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/models/user';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+  baseUrl = environment.apiUrl;
+  private _currentUserSource = new ReplaySubject<IUser>(1)
+  currentUser$ = this._currentUserSource.asObservable();
+
+  constructor(private http: HttpClient,
+              private router: Router) { }  
+  
+  loadCurrentUser(token: string) {
+    if(token === null){
+      this._currentUserSource.next(null)
+      return of(null)
+    }
+
+    let headers = new HttpHeaders()
+    headers = headers.set('Authorization', `Bearer ${token}`)
+
+    return this.http.get(this.baseUrl + 'account', {headers})
+      .pipe(
+        map((user: IUser) => {
+          if(user)
+            localStorage.setItem('token', user.token)
+            this._currentUserSource.next(user)
+        })
+      )
+  }
+
+  login(values: any){
+    return this.http.post(this.baseUrl + 'account/login', values)
+      .pipe(
+        map((user: IUser) => {
+          if(user){
+            localStorage.setItem('token', user.token)
+            this._currentUserSource.next(user)
+          }
+        })
+      )
+  }
+
+  register(values: any){
+    return this.http.post(this.baseUrl + 'account/register', values)
+    .pipe(
+      map((user: IUser) => {
+        if(user){
+          localStorage.setItem('token', user.token)
+          this._currentUserSource.next(user)
+        }
+      })
+    )
+  }
+
+  logout(){
+    localStorage.removeItem('token')
+    this._currentUserSource.next(null)
+    this.router.navigateByUrl("/")
+  }
+
+  checkEmailExists(email: string){
+    return this.http.get(this.baseUrl + 'account/emailexists?email='+ email)
+  }
+}
