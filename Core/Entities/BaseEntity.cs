460e5bc79ccacf2288119f98cd7a9f37295bc92c
Rodrigo Abreu <rodrigo.abreu@reditpro.com>
2021-12-31 10:59:30 +0000

    End of section 18

--- Core/Entities/BaseEntity.cs
+++ Core/Entities/BaseEntity.cs
@@ -1,12 +1,5 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Core.Entities
-{
-    public class BaseEntity
-    {
+namespace Core.Entities {
+    public class BaseEntity {
         public int Id { get; set; }
     }
 }
